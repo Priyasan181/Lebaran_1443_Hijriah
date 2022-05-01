40621b18e3d54c8511c72909302b7ecf5ddb8034
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-05-01 23:37:21 +0700

    musik

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-window.addEventListener('load', () => {
-    Swal.fire({
-      title: 'Do you want to play music in the background?',
-      // text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'No',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        document.querySelector('.song').play();
-        resolveFetch().then(animationTimeline());
-      } else {
-        resolveFetch().then(animationTimeline());
-      }
-    });
-  });  
-
 const Iya = document.querySelector(".next");
 const Text = document.getElementById("text");
 const Image = document.querySelector("#image-gif");
